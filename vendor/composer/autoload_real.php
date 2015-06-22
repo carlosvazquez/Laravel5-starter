fd4c55812cfe5abcf4c10dfc6cfb8d92dbbb5373
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2015-06-22 15:44:19 -0500

    update database by migrations

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInitb5f825fe02feb91b56f35136031baf24
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitb5f825fe02feb91b56f35136031baf24', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
