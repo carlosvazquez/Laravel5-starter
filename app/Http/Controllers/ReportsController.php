b59b40bdae0ab95103e71ce9a04d80605b528cf0
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2015-06-23 18:26:50 -0500

    fail fixed database boolean to string column dto in reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -179,6 +179,7 @@ class ReportsController extends Controller {
         $data = Report::find($id);
 
         $data->fill(Request::all());
+
         $data->save();
 
         $data = Report::find($id);
